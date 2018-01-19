fdfac0b8daf398925ff3c89041cacc212678d19d
dinkodonchev <dinyo.donchev@abv.bg>
2018-01-19 20:44:38 +0200

    This is my Game book about adventures in the Mountain

---
+++ GameBookTheGreatMountainHike/GreatMountainHike.cs
@@ -0,0 +1,95 @@
+﻿
+
+namespace GameBookTheGreatMountainHike
+{
+    using GameBookTheGreatMountainHike.HeroModels;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class GreatMountainHike
+    {
+        static void Main(string[] args)
+        {
+            ArchModel Hero = new ArchModel();
+            string charClass = "";
+
+            Console.WriteLine("Would you like to choose your hero class or do you leave it at the hands of Faith?");
+            Console.WriteLine();
+            Console.WriteLine("To choose - press Y. To leave it at random - press N.");
+            Console.WriteLine();
+            ConsoleKeyInfo BirthChoice = Console.ReadKey();
+            Console.WriteLine();
+
+            if (BirthChoice.Key == ConsoleKey.Y)
+            {
+                Console.WriteLine("For Shepherd, press S! For Hunter, press H. For Mountaineer, press M. For Adventurous Explorer, press A. Type a correspondig letter to pick a class:");
+                Console.WriteLine();
+                string choice = Console.ReadLine();
+
+
+                switch (choice.ToLower())
+                {
+                    case "s":
+                        Hero = new ShepherdModel();
+                        charClass = "Shepherd";
+                        break;
+                    case "h":
+                        Hero = new HunterModel();
+                        charClass = "Hunter";
+                        break;
+                    case "m":
+                        Hero = new Mountaineer();
+                        charClass = "Mountaineer";
+                        break;
+                    case "a":
+                        Hero = new AdventurousExplorer();
+                        charClass = "Adventurous  Explorer";
+                        break;
+                    default:
+                        Hero = new Mountaineer();
+                        break;
+                }
+            }
+
+            else if (BirthChoice.Key == ConsoleKey.N)
+            {
+                
+                Random randHeroClass = new Random();
+                int OrisiyaFaith = randHeroClass.Next(1, 4);
+
+                switch (OrisiyaFaith)
+                {
+                    case 1:
+                        Hero = new ShepherdModel();
+                        charClass = "Shepherd";
+                        break;
+                    case 2:
+                        Hero = new HunterModel();
+                        charClass = "Hunter";
+                        break;
+                    case 3:
+                        Hero = new Mountaineer();
+                        charClass = "Mountaineer";
+                        break;
+                    case 4:
+                        Hero = new AdventurousExplorer();
+                        charClass = "Adventourous Explorer";
+                        break;
+                    default:
+                        Hero = new Mountaineer();
+                        break;
+                }
+            }
+            var indefiniteArticle = charClass == "Adventourous Explorer" ? "an" : "a";
+            Console.WriteLine("Congrats! You have been born {0} {1}.", indefiniteArticle, charClass);
+            Console.WriteLine();
+            var heroType = Hero.GetType();
+            Console.WriteLine(heroType);
+
+            
+        }
+    }
+}
---
+++ GameBookTheGreatMountainHike/HeroModels/ShepherdModel.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameBookTheGreatMountainHike.HeroModels
+{
+    class ShepherdModel : ArchModel
+    {
+        public ShepherdModel()
+        {
+
+        }
+
+        //public class Herd
+        //{
+        //    public int InitialHerd()
+        //    {
+        //        Random random = new Random();
+        //        int numberOfFlock = random.Next(8, 200);
+        //        return numberOfFlock;
+
+        //    }
+        //}
+
+        //public class Sheperd
+        //{
+        //    private int wealth = 0;
+        //    private int experience = 0;
+
+        //    public int Wealth
+        //    {
+
+        //        get
+        //        {
+        //            return this.wealth;
+        //        }
+
+        //        set
+        //        {
+
+        //            this.wealth = value;
+        //        }
+        //    }
+        //}
+
+        //public class Ovcharche
+        //{
+
+
+        //    static void Main(string[] args)
+        //    {
+        //        Herd herd = new Herd();
+        //        int flock = herd.InitialHerd();
+        //        Sheperd sheperd = new Sheperd();
+        //        Console.WriteLine("The shepperd starts with a flock of {0} sheep.", flock);
+        //        if (flock < 50)
+        //        {
+        //            sheperd.Wealth = 1;
+        //            Console.WriteLine("This little sheperd is rather poor for the time being.");
+        //        }
+        //        else if (flock > 50 && flock < 120)
+        //        {
+        //            sheperd.Wealth = 2;
+        //            Console.WriteLine("This little sheperd is rather medium welathy for the time being.");
+        //        }
+        //        else
+        //        {
+        //            sheperd.Wealth = 3;
+        //            Console.WriteLine("This little sheperd is rather rich for the time being.");
+        //        }
+        //    }
+
+
+        //}
+    }
+}
